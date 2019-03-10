dbda95e4546abcc846ebc87576b83bf281f25ff6
Luis Eduardo Yamin Lacouture <lyamin@uniandes.edu.co>
2019-03-10 17:19:25 -0500

    Implementación Pruebas

--- test/pruebasColaPrioridad/pruebaColaPrioridad.java
+++ test/pruebasColaPrioridad/pruebaColaPrioridad.java
@@ -48,89 +48,89 @@ public class pruebaColaPrioridad extends TestCase {
 
 	}
 
-//	public void testColaVaciaAgregoyEliminoelementos(){
-//		setUpEscenario0();
-//
-//		//Agrego elementos a las colas
-//		LocationVO elemento1 = new LocationVO(815694, "100 BLK MICHIGAN AVE NW E/B", 10);
-//		LocationVO elemento2 = new LocationVO(814983,"3RD ST TUNNEL NW N/B BY MA AVE",20);
-//		LocationVO elemento3 = new LocationVO(805065,"2200 BLOCK K ST NW E/B",40);
-//
-//		cp1.agregar(elemento1);
-//		cp1.agregar(elemento2);
-//		cp1.agregar(elemento3);
-//
-//		cp2.agregar(elemento1);
-//		cp2.agregar(elemento2);
-//		cp2.agregar(elemento3);
-//
-//		//Ahora se verifica que se elimine el máximo correctamente		
-//		assertEquals("El elemento máximo de MaxCP es el número 3",elemento3, cp1.max());
-//		assertEquals("El elemento máximo de HeapCP es el número 3",elemento3, cp2.max());
-//		assertEquals("Debería eliminarse de MaxCP el elemento 3",elemento3, cp1.delMax());
-//		assertEquals("Debería eliminarse de HepCP el elemento 3",elemento3, cp2.delMax());
-//		assertEquals("Debería eliminarse de MaxCP el elemento 2",elemento2, cp1.delMax());
-//		assertEquals("Debería eliminarse de HepCP el elemento 2",elemento2, cp2.delMax());
-//		assertEquals("Debería eliminarse de MaxCP el elemento 1",elemento1, cp1.delMax());
-//		assertEquals("Debería eliminarse de HepCP el elemento 1",elemento1, cp2.delMax());
-//
-//
-//	}
-//
-//	public void testMuchosElementos(){
-//
-//		setUpEscenario1();
-//		//Se verifica que las colas contengan todos los datos
-//		assertEquals("Debería haber 100 elementos",100, cp1.darNumElementos());
-//		assertEquals("Debería haber 100 elementos",100, cp2.darNumElementos());
-//
-//
-//		//Se verifica que estén ordenados las colas
-//		assertEquals("El elemento máximo no debería ser ese",109, cp1.max().getNumberOfRegisters());
-//		assertEquals("El elemento máximo no debería ser ese",109, cp2.max().getNumberOfRegisters());
-//
-//		//Se verifica que se eliminen todos los elementos en orden
-//		for (int i = 0; i < 100; i++) {
-//			assertEquals("Los elementos no se están eliminando en orden",(99-i)+10, cp1.delMax().getNumberOfRegisters());
-//			assertEquals("Los elementos no se están eliminando en orden",(99-i)+10, cp2.delMax().getNumberOfRegisters());
-//		}
-//
-//	}
-//
-//
-//	public void agregarNuevoElementosConMuchosElementos(){
-//
-//		setUpEscenario1();
-//
-//		//Insertando un elemento muy grande
-//		LocationVO auxiliar = new LocationVO(123, "NA", 1000000);
-//		cp1.agregar(auxiliar);
-//		cp2.agregar(auxiliar);
-//
-//		//Debe eliminarse el elemento recién agregado
-//		assertEquals("El elemento eliminado no debería ser ese",auxiliar, cp1.delMax());
-//		assertEquals("El elemento eliminado no debería ser ese",auxiliar, cp2.delMax());
-//
-//
-//		//Insertando un elemento con la misma prioridad
-//		auxiliar = new LocationVO(123,"NA",109);
-//		LocationVO auxiliar2 = new LocationVO(123, "NA", 99);
-//		cp1.agregar(auxiliar);
-//		cp2.agregar(auxiliar);
-//		cp1.agregar(auxiliar2);
-//		cp2.agregar(auxiliar2);
-//
-//		assertEquals("El elemento eliminado no debería ser ese",109, cp1.delMax().getNumberOfRegisters());
-//		assertEquals("El elemento eliminado no debería ser ese",109, cp1.delMax().getNumberOfRegisters());
-//		assertEquals("El elemento eliminado no debería ser ese",109, cp2.delMax().getNumberOfRegisters());
-//		assertEquals("El elemento eliminado no debería ser ese",109, cp2.delMax().getNumberOfRegisters());
-//
-//
-//		assertEquals("El elemento eliminado no debería ser ese",99, cp1.delMax().getNumberOfRegisters());
-//		assertEquals("El elemento eliminado no debería ser ese",99, cp1.delMax().getNumberOfRegisters());
-//		assertEquals("El elemento eliminado no debería ser ese",99, cp2.delMax().getNumberOfRegisters());
-//		assertEquals("El elemento eliminado no debería ser ese",99, cp2.delMax().getNumberOfRegisters());
-//
-//	}
+	public void testColaVaciaAgregoyEliminoelementos(){
+		setUpEscenario0();
+
+		//Agrego elementos a las colas
+		LocationVO elemento1 = new LocationVO(815694, "100 BLK MICHIGAN AVE NW E/B", 10);
+		LocationVO elemento2 = new LocationVO(814983,"3RD ST TUNNEL NW N/B BY MA AVE",20);
+		LocationVO elemento3 = new LocationVO(805065,"2200 BLOCK K ST NW E/B",40);
+
+		cp1.agregar(elemento1);
+		cp1.agregar(elemento2);
+		cp1.agregar(elemento3);
+
+		cp2.agregar(elemento1);
+		cp2.agregar(elemento2);
+		cp2.agregar(elemento3);
+
+		//Ahora se verifica que se elimine el máximo correctamente		
+		assertEquals("El elemento máximo de MaxCP es el número 3",elemento3, cp1.max());
+		assertEquals("El elemento máximo de HeapCP es el número 3",elemento3, cp2.max());
+		assertEquals("Debería eliminarse de MaxCP el elemento 3",elemento3, cp1.delMax());
+		assertEquals("Debería eliminarse de HepCP el elemento 3",elemento3, cp2.delMax());
+		assertEquals("Debería eliminarse de MaxCP el elemento 2",elemento2, cp1.delMax());
+		assertEquals("Debería eliminarse de HepCP el elemento 2",elemento2, cp2.delMax());
+		assertEquals("Debería eliminarse de MaxCP el elemento 1",elemento1, cp1.delMax());
+		assertEquals("Debería eliminarse de HepCP el elemento 1",elemento1, cp2.delMax());
+
+
+	}
+
+	public void testMuchosElementos(){
+
+		setUpEscenario1();
+		//Se verifica que las colas contengan todos los datos
+		assertEquals("Debería haber 100 elementos",100, cp1.darNumElementos());
+		assertEquals("Debería haber 100 elementos",100, cp2.darNumElementos());
+
+
+		//Se verifica que estén ordenados las colas
+		assertEquals("El elemento máximo no debería ser ese",109, cp1.max().getNumberOfRegisters());
+		assertEquals("El elemento máximo no debería ser ese",109, cp2.max().getNumberOfRegisters());
+
+		//Se verifica que se eliminen todos los elementos en orden
+		for (int i = 0; i < 100; i++) {
+			assertEquals("Los elementos no se están eliminando en orden",(99-i)+10, cp1.delMax().getNumberOfRegisters());
+			assertEquals("Los elementos no se están eliminando en orden",(99-i)+10, cp2.delMax().getNumberOfRegisters());
+		}
+
+	}
+
+
+	public void agregarNuevoElementosConMuchosElementos(){
+
+		setUpEscenario1();
+
+		//Insertando un elemento muy grande
+		LocationVO auxiliar = new LocationVO(123, "NA", 1000000);
+		cp1.agregar(auxiliar);
+		cp2.agregar(auxiliar);
+
+		//Debe eliminarse el elemento recién agregado
+		assertEquals("El elemento eliminado no debería ser ese",auxiliar, cp1.delMax());
+		assertEquals("El elemento eliminado no debería ser ese",auxiliar, cp2.delMax());
+
+
+		//Insertando un elemento con la misma prioridad
+		auxiliar = new LocationVO(123,"NA",109);
+		LocationVO auxiliar2 = new LocationVO(123, "NA", 99);
+		cp1.agregar(auxiliar);
+		cp2.agregar(auxiliar);
+		cp1.agregar(auxiliar2);
+		cp2.agregar(auxiliar2);
+
+		assertEquals("El elemento eliminado no debería ser ese",109, cp1.delMax().getNumberOfRegisters());
+		assertEquals("El elemento eliminado no debería ser ese",109, cp1.delMax().getNumberOfRegisters());
+		assertEquals("El elemento eliminado no debería ser ese",109, cp2.delMax().getNumberOfRegisters());
+		assertEquals("El elemento eliminado no debería ser ese",109, cp2.delMax().getNumberOfRegisters());
+
+
+		assertEquals("El elemento eliminado no debería ser ese",99, cp1.delMax().getNumberOfRegisters());
+		assertEquals("El elemento eliminado no debería ser ese",99, cp1.delMax().getNumberOfRegisters());
+		assertEquals("El elemento eliminado no debería ser ese",99, cp2.delMax().getNumberOfRegisters());
+		assertEquals("El elemento eliminado no debería ser ese",99, cp2.delMax().getNumberOfRegisters());
+
+	}
 
 }
